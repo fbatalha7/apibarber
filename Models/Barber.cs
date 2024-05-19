bda3eb5ff118d9090a96da32b4825524b696cec5
Felipe Batalha <felipebatalhanasc@gmail.com>
2024-05-18 23:32:58 -0300

    First Deploy Project for app barber to University

---
+++ Models/Barber.cs
@@ -0,0 +1,25 @@
+﻿using barberapi.Models;
+
+namespace barberapi.Models
+{
+    public class Barber
+    {
+        public string? Name { get; set; }
+        public double Stars { get; set; }
+        public string? City { get; set; }
+        public List<Barber>? ListAvailable { get; set;}
+        public List<Barber> DadosListaDisponivel()
+        {
+            if (ListAvailable?.Count == 0 || ListAvailable is null)
+            {
+                ListAvailable?.Add(new Barber { City = "Coari", Stars = 4.5, Name = "ADMIM TESTE" });
+                ListAvailable?.Add(new Barber { City = "Anori", Stars = 4.5, Name = "ADMIM TESTE" });
+                ListAvailable?.Add(new Barber { City = "Manaus", Stars = 4.5, Name = "ADMIM TESTE" });
+            }
+
+            return ListAvailable;
+        }
+
+    }
+}
+
