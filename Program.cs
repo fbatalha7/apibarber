b2cbef6935585ee412aa5dab987d20bbaba4bbed
Felipe Batalha <felipebatalhanasc@gmail.com>
2024-05-27 00:29:57 -0300

    configure swagger

--- Program.cs
+++ Program.cs
@@ -1,20 +1,32 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Funções do Azure API",
+        Description = "Documentação da API das Funções do Azure",
+        Contact = new OpenApiContact
+        {
+            Name = "Nome do seu contato",
+            Email = "email@exemplo.com"
+        }
+    });
+});
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
