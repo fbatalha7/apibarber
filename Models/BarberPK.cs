c1057581b4304375438ea99ed69c365fbae25503
Felipe Batalha <felipebatalhanasc@gmail.com>
2024-06-09 21:22:45 -0300

    feat get barber

---
+++ Models/BarberPK.cs
@@ -0,0 +1,12 @@
+﻿namespace barberapi.Models
+{
+    public class BarberPK
+    {
+        public int Id { get; set; }
+        public bool favorited { get; set; }
+        public List<string> photos { get; set; }
+        public List<string> services { get; set; }
+
+
+    }
+}
