00848fa7afacabb2ecb7a90a0997d1c4c1657b6b
Felipe Batalha <felipebatalhanasc@gmail.com>
2024-06-11 21:39:32 -0300

    services

--- Models/BarberPK.cs
+++ Models/BarberPK.cs
@@ -5,8 +5,15 @@
         public int Id { get; set; }
         public bool favorited { get; set; }
         public List<string> photos { get; set; }
-        public List<string> services { get; set; }
+        public List<Services> services { get; set; }
 
+
+    }
+
+    public class Services
+    {
+        public string corte { get; set; }
+        public double price { get; set; }
 
     }
 }
